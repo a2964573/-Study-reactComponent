390f24a8a19333cdcf80629a1605cf2d43c8dba3
a2964573 <85636742+a2964573@users.noreply.github.com>
2022-07-11 15:17:45 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(props){
+  return <header>
+  <h1>
+    {props.title}
+  </h1>
+  </header>
+}
+function Content(props){
+  const lis=[];
+  for(let i=0; i<props.topics.length; i++){
+    let t=props.topics[i];
+    lis.push(<li key={t.id}><a href={t.rink} target={t.target}>{t.title}</a></li>);
+  }
+  return <nav>
+  <h2>
+    <ol>
+      {lis}
+    </ol>
+  </h2>
+  </nav>
+}
+function Footer(){
+  return <footer>
+  링크를 클릭하면 사이트가 새 창으로 열립니다.
+  </footer>
+}
+
 function App() {
+  const topics=[
+    {id:1, title:"네이버 바로가기", rink:"https://www.naver.com", target:"_blank"},
+    {id:2, title:"구글 바로가기", rink:"https://www.google.com", target:"_blank"},
+    {id:3, title:"다음 바로가기", rink:"https://www.daum.net", target:"_blank"}
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="WEBSITE REDIRECT"></Header>
+      <Content topics={topics}></Content>
+      <Footer></Footer>
     </div>
   );
 }
